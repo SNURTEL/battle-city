dbb06b7fecd9d244ae870d4472ea0c3e407ed475
Rafal <rafal5094@gmail.com>
2022-05-22 21:41:11 +0200

    Added more connections between states

--- src/game-lib/FinishedEventHandler.cpp
+++ src/game-lib/FinishedEventHandler.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #include "include/FinishedEventHandler.h"
+#include "include/Game.h"
 #include "../core-lib/include/EventHandler.h"  //FIXME SUPER UGLY
 
 
@@ -17,11 +18,14 @@ void FinishedEventHandler::processEvent(std::unique_ptr<Event> event) {
     switch (event->type) {
         case(Event::KeyPressed): {
             //TODO Implement response to KeyPressed
-            std::cout << "Key pressed: " << event->info.keyInfo.keyCode << std::endl;
+            std::cout << "State is finished! " << event->info.keyInfo.keyCode << std::endl;
             break;
         }
         case (Event::KeyReleased):{
-            std::cout << "Key released: " << event->info.keyInfo.keyCode << std::endl;
+            if (event->info.keyInfo.keyCode == 36) {
+                game_->quit();
+            }
+            std::cout << "State is finished! " << event->info.keyInfo.keyCode << std::endl;
             break;
         }
         case (Event::NullEvent):{
