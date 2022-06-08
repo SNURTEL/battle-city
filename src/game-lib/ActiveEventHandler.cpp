eb8a155bc0e40174466052fcac3d824a7a79ca31
Rafal <rafal5094@gmail.com>
2022-06-08 10:30:24 +0200

    deleted snapTankToGrid when stopped moving player

--- src/game-lib/ActiveEventHandler.cpp
+++ src/game-lib/ActiveEventHandler.cpp
@@ -316,22 +316,22 @@ std::unique_ptr<Event> ActiveEventHandler::processEvent(std::unique_ptr<Event> e
            // DOWN
            if(event->info.keyInfo.keyCode == 74) {
                game_->getBoard()->setTankMoving(game_->getBoard()->getPlayerTank(), false);
-               game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
+               //game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
            }
            // UP
            if(event->info.keyInfo.keyCode == 73) {
                game_->getBoard()->setTankMoving(game_->getBoard()->getPlayerTank(), false);
-               game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
+               //game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
            }
            // LEFT
            if(event->info.keyInfo.keyCode == 72) {
                game_->getBoard()->setTankMoving(game_->getBoard()->getPlayerTank(), false);
-               game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
+               //game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
            }
            // RIGHT
            if(event->info.keyInfo.keyCode == 71) {
                game_->getBoard()->setTankMoving(game_->getBoard()->getPlayerTank(), false);
-               game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
+               //game_->getBoard()->snapTankToGrid(game_->getBoard()->getPlayerTank());
            }
            // SPACEBAR
            if(event->info.keyInfo.keyCode == 57) {
--- src/game-lib/Menu.cpp
+++ src/game-lib/Menu.cpp
@@ -8,7 +8,6 @@
 
 Menu::Menu(unsigned int max_pos) {
     max_pos_ = max_pos;
-    pos_ = max_pos;
 }
 
 void Menu::add_pos() {
