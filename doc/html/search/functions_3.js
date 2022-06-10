de0c6b72f2b3537c5fb447499f20613ea864bf96
Tomasz Owienko <tomek.owienko@gmail.com>
2022-06-10 20:59:51 +0200

    Re-generate doxygen

--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -5,7 +5,7 @@ var searchData=
   ['deltalives_2',['deltaLives',['../class_tank.html#a3d887009683a21cff4a7cf6fc490f675',1,'Tank']]],
   ['deregisterallbots_3',['deregisterAllBots',['../class_bot_controller.html#ac4df97d199bf92847c6032039c25d533',1,'BotController']]],
   ['deregisterbot_4',['deregisterBot',['../class_bot_controller.html#a7144260380b33679b75fe858a0cd9d8b',1,'BotController']]],
-  ['destroybulletcollisiontiles_5',['destroyBulletCollisionTiles',['../_active_event_handler_8cpp.html#a2fe9af6896a8971b5c8740423719ed25',1,'ActiveEventHandler.cpp']]],
+  ['destroybulletcollisiontiles_5',['destroyBulletCollisionTiles',['../_active_event_handler_8cpp.html#a8b450afa2bee545cc324bd409dcc00ea',1,'ActiveEventHandler.cpp']]],
   ['detacheventsubscriber_6',['detachEventSubscriber',['../class_event_publisher.html#a5bfeea787cb42e37b4673bbc269f93e2',1,'EventPublisher']]],
   ['detachsubscriber_7',['detachSubscriber',['../class_simple_publisher.html#aeaeefe6ed2589c12a809e1dbd3d03102',1,'SimplePublisher']]]
 ];
